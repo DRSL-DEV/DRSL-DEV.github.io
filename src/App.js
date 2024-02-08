c90e62aeadce93cb315b34300f7c6df671b6786e
Xinran <xinrancao@hotmail.com>
2024-02-08 12:05:26 -0500

    basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import StoryPage from "./pages/story/StoryPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/story" element={<StoryPage />} />
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/story/StoryPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StoryPage = () => {
+  return <div>StoryPage</div>;
+};
+
+export default StoryPage;
