20063d33f297ffb2b21328bd080e6853003dde32
Xinran <xinrancao@hotmail.com>
2024-02-13 12:09:34 -0500

    update footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,15 +6,18 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="links">
-        <Link to="/contact">Contact</Link>
-        <Link to="/partnerships">Partnerships</Link>
-        <Link to="/search">Search</Link>
         <Link to="/about">About Us</Link>
+        <Link to="/partnerships">Partnerships</Link>
+        <Link to="/contact">Contact</Link>
         <Link to="/privacy">Privacy Statement</Link>
       </div>
       <div className="bottom-info">
         <img src={logo_with_text} alt="Logo" className="footer-logo" />
-        <span>@2024 Detroit River Story Lab. All rights reserved.</span>
+        <span>
+          @2024 Detroit River Story Lab.
+          <br />
+          All rights reserved.
+        </span>
       </div>
     </footer>
   );
