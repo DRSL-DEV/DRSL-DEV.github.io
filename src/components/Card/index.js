b8ce36b222dab723ed84b6942a3d6738154751ca
oliviahyw <112413539+oliviahyw@users.noreply.github.com>
2024-02-15 09:17:54 -0500

    fixed classname

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 
 const Card = ({ title, content, author, imgSrc, type }) => {
-  const cardClass = `card ${type}`;
+  const cardClass = `customized-card ${type}`;
 
   return (
     <div className={cardClass}>
-      <div className="card-image">
+      <div className="customized-card-image">
         <img src={imgSrc} alt="" />
       </div>
-      <div className="card-content">
+      <div className="customized-card-content">
         <h2>{title}</h2>
         <p>{content}</p>
         <div className="customized-card-footer">
