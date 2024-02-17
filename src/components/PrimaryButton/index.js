3fe710261d14716cde56c466f2b0d4e292105e6e
oliviahyw <112413539+oliviahyw@users.noreply.github.com>
2024-02-17 10:57:05 -0500

    Used antd for input components

--- src/components/PrimaryButton/index.js
+++ src/components/PrimaryButton/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './index.css';
 
-const PrimaryButton = ({ text, onClick }) => {
+const PrimaryButton = ({ text, htmlType, onClick }) => {
     return (
-        <button className="primary-button" onClick={onClick}>
+        <button className="primary-button" type={htmlType} onClick={onClick}>
             {text}
         </button>
     );
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,31 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import back from "../../assets/icons/back.svg";
 import { Link } from "react-router-dom";
-import { Form, InputGroup, Row, Col } from 'react-bootstrap';
 import PrimaryButton from "../../components/PrimaryButton";
 import GoogleIcon from "../../assets/icons/Google icon.svg";
 import FacebookIcon from "../../assets/icons/Facebook icon.svg";
 import TwitterIcon from "../../assets/icons/Twitter icon.svg";
+import PasswordInput from "../../components/PasswordInput";
+import EmailInput from "../../components/EmailInput";
+import { Form } from 'antd';
 
 const LoginPage = () => {
-    const [credentials, setCredentials] = useState({ username: '', password: '' });
-    const [rememberMe, setRememberMe] = useState(false);
+    const [form] = Form.useForm();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setCredentials(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-
-    const handleRememberMeChange = (e) => {
-        setRememberMe(e.target.checked);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(credentials);
+    const handleSubmit = (values) => {
+        console.log(values);
     };
 
     return (
@@ -39,50 +27,23 @@ const LoginPage = () => {
                 <h1 className="pageTitle">Login</h1>
             </div>
             <p className="pageDescription">Login or Sign up to continue.</p>
-            <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <InputGroup>
-                        <Form.Control
-                            type="email"
-                            placeholder="Username or Email"
-                            className="input"
-                            name="username"
-                            value={credentials.username}
-                            onChange={handleChange}
-                        />
-                    </InputGroup>
-                </Form.Group>
+            <Form form={form} onFinish={handleSubmit}>
+                <Form.Item
+                    name="email"
+                    rules={[
+                        { required: true, message: 'Please input your email' },
+                        { type: 'email', message: 'Please enter a valid email' },
+                    ]}
+                >
+                    <EmailInput placeholder="Email" />
+                </Form.Item>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <InputGroup>
-                        <Form.Control
-                            type="password"
-                            placeholder="Password"
-                            className="input"
-                            name="password"
-                            value={credentials.password}
-                            onChange={handleChange}
-                        />
-                    </InputGroup>
-                </Form.Group>
+                <PasswordInput
+                    name="password"
+                    placeholder="Password"
+                />
 
-                <Row className="mb-3 justify-content-between align-items-center">
-                    <Col xs="auto">
-                        <Form.Check
-                            type="checkbox"
-                            id="rememberMe"
-                            label="Remember me"
-                            className="help-text"
-                            onChange={handleRememberMeChange}
-                            checked={rememberMe}
-                        />
-                    </Col>
-                    <Col xs="auto">
-                        <Link to="/forgot-password" className="help-text">Forget password?</Link>
-                    </Col>
-                </Row>
-
-                <PrimaryButton text="Login" onClick={handleSubmit} />
+                <PrimaryButton text="Login" htmlType="submit" />
             </Form>
             <p className="help-text">
                 Don't have an account?
