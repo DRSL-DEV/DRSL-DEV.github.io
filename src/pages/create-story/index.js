5f1733b1f18260fcd8b64d74b1c4f5d215f618fb
Ceciel Zhong <xizhong@umich.edu>
2024-02-20 02:24:06 -0500

    form file logic

--- src/pages/create-story/index.js
+++ src/pages/create-story/index.js
@@ -1,4 +1,4 @@
-import { Form, Input, Upload } from "antd";
+import { Form, Input, Upload, message } from "antd";
 import StoryHeader from "../../components/PageHeader";
 import Button from "../../components/Button";
 import upload_icon from "../../assets/icons/upload_icon.svg";
@@ -9,11 +9,28 @@ const CreateStory = () => {
     console.log("Received values of form: ", values);
   };
 
+  const onFileChange = (info) => {
+    console.log('file status change', info);
+  
+    // Log details if the file is successfully read
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+      // here: any callback to trigger after successful upload
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  };
+
   const uploadProps = {
     beforeUpload: (file) => {
-      // Implement file handling logic here
+      console.log("File details:", {
+        name: file.name,
+        type: file.type,
+        size: `${(file.size / 1024 / 1024).toFixed(2)} MB`,
+      });
       return false;
     },
+    onChange: onFileChange,
   };
 
   return (
