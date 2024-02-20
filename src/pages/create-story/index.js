130057bc81285c190da3c71921b0911c567f4b16
Ceciel Zhong <xizhong@umich.edu>
2024-02-20 02:47:16 -0500

    added file type  validation rules

--- src/pages/create-story/index.js
+++ src/pages/create-story/index.js
@@ -4,6 +4,18 @@ import Button from "../../components/Button";
 import upload_icon from "../../assets/icons/upload_icon.svg";
 import "./index.css";
 
+const allowedFileTypes = [
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+  'video/mp4',
+  'video/webm',
+  'video/ogg',
+  'audio/mpeg',
+  'audio/ogg',
+  'audio/wav',
+];
+
 const CreateStory = () => {
   const onFinish = (values) => {
     console.log("Received values of form: ", values);
@@ -15,7 +27,7 @@ const CreateStory = () => {
     // Log details if the file is successfully read
     if (info.file.status === 'done') {
       message.success(`${info.file.name} file uploaded successfully`);
-      // here: any callback to trigger after successful upload
+      // Here: any callback to trigger after successful upload
     } else if (info.file.status === 'error') {
       message.error(`${info.file.name} file upload failed.`);
     }
@@ -23,11 +35,17 @@ const CreateStory = () => {
 
   const uploadProps = {
     beforeUpload: (file) => {
-      console.log("File details:", {
-        name: file.name,
-        type: file.type,
-        size: `${(file.size / 1024 / 1024).toFixed(2)} MB`,
-      });
+      if (allowedFileTypes.includes(file.type)) {
+        console.log("File details:", {
+          name: file.name,
+          type: file.type,
+          size: `${(file.size / 1024 / 1024).toFixed(2)} MB`,
+        });
+      } 
+      else {
+        message.error('You can only upload image, video, or audio files!');
+        return Upload.LIST_IGNORE;
+      }
       return false;
     },
     onChange: onFileChange,
