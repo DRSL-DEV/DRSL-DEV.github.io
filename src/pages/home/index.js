88e763176615ede91ae82b06bc0f32801ceea5b1
oliviahyw <112413539+oliviahyw@users.noreply.github.com>
2024-02-21 19:44:48 -0500

    Added home carousel

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,15 +2,35 @@ import styles from "./index.module.css";
 import Card from "../../components/Card";
 import Button from "../../components/Button";
 import imgSrc from "../../assets/images/card_img.png";
+import gallery_placeholder from "../../assets/images/home_gallery.png";
 import { useNavigate } from "react-router-dom";
+import { Carousel } from "antd";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
+  const mediaUrls = [gallery_placeholder, gallery_placeholder, gallery_placeholder];
+
   return (
-    <div className="page-container">
+    <div className={`page-container ${styles["homepage-container"]}`}>
       <main>
-        <section className={styles["user-stories"]}>
+        <Carousel className={styles.carousel} autoplay>
+          {mediaUrls.map((mediaUrl, index) => (
+            <div key={index} className={styles["carousel-item"]}>
+              <img
+                className={styles["story-image-gallery"]}
+                src={mediaUrl}
+                alt="home gallery"
+              />
+              <div className={styles.overlay}>
+                <p className={styles["carousel-title"]}>Historical Collective</p>
+                <p className={styles["carousel-sub-title"]}>Reflective Perspectives from Detroit Natives</p>
+                <a href="/story" onClick={() => navigate("/story")} className={styles["carousel-link"]}>Learn More</a>
+              </div>
+            </div>
+          ))}
+        </Carousel>
+        <section className={styles["user-stories-section"]}>
           <h2 className={styles["story-h2"]}>Site 3: User Stories</h2>
           <div className={styles["cards-container"]}>
             <Card
@@ -32,11 +52,11 @@ const HomePage = () => {
                 text="Share Your Story"
                 handleOnClick={() => navigate("/create-story")}
               />
-              <Button text="View More" handleOnClick={() => {}} />
+              <Button text="View More" handleOnClick={() => { }} />
             </div>
           </div>
         </section>
-        <section className={styles["partnered-stories"]}>
+        <section className={styles["partnered-stories-section"]}>
           <h2 className={styles["story-h2"]}>Site 3: Partnered Stories</h2>
           <div className={styles["cards-container"]}>
             <Card
