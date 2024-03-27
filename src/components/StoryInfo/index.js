c6f8f023d69715117a6d50df73ad0ccf2e43a344
Xinran <xinrancao@hotmail.com>
2024-03-27 16:27:31 -0400

    anonymous username & default profile pic

--- src/components/StoryInfo/index.js
+++ src/components/StoryInfo/index.js
@@ -1,4 +1,5 @@
 import styles from "./index.module.css";
+import profile from "../../assets/images/profile.png";
 import { convertTimeFormatMDY } from "../../utils/dateFormat";
 
 const StoryInfo = ({
@@ -14,9 +15,9 @@ const StoryInfo = ({
     <div className={styles["story-info-container"]}>
       <div className={styles["post-info-top"]}>
         <div className={styles["user-info"]}>
-          <img src={profileImg} alt="profile image" />
+          <img src={profileImg || profile} alt="profile image" />
           <div className={styles["user-info-text"]}>
-            <h6>{anonymous ? "anonymous" : author}</h6>
+            <h6>{anonymous ? "Anonymous" : author}</h6>
             <h6>{convertTimeFormatMDY(date)}</h6>
           </div>
         </div>
