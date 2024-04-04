558ce04f3488951612c0768d999eef1878bc9dc3
Xinran <xinrancao@hotmail.com>
2024-04-04 01:35:40 -0400

    fix: admin card default media image

--- src/components/AdminCard/index.js
+++ src/components/AdminCard/index.js
@@ -1,15 +1,24 @@
 import styles from "./index.module.css";
+import { useState } from "react";
 import arrow_right from "../../assets/icons/arrow_right.svg";
+import defaultImg from "../../assets/images/default_media.png";
 import { useNavigate } from "react-router-dom";
 
 const AdminCard = ({ storyInfo }) => {
+  const [hasError, setHasError] = useState(false);
   const { user, title, content, media, postType } = storyInfo;
   const navigate = useNavigate();
 
   return (
     <div className={`${styles["customized-card"]} ${styles[postType]}`}>
       <div className={styles["customized-card-image"]}>
-        {!!media.length && media[0] && <img src={media[0]} alt="" />}
+        {!!media.length && media[0] && (
+          <img
+            src={hasError ? defaultImg : media[0]}
+            onError={() => setHasError(true)}
+            alt=""
+          />
+        )}
       </div>
       <div className={styles["customized-card-content"]}>
         <h2>{title}</h2>
