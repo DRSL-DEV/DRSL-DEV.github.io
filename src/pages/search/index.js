40990310e40e72b9dc70bea80cb28e44683baeae
Xinran <xinrancao@hotmail.com>
2024-04-04 00:55:19 -0400

    fix search

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,13 +1,13 @@
 import styles from "./index.module.css";
-import { Input } from 'antd';
+import { Input } from "antd";
 import search from "../../assets/icons/search.svg";
 import filter from "../../assets/icons/filter.svg";
-import back from "../../assets/icons/back.svg"
+import back from "../../assets/icons/back.svg";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Select, Radio, Divider, Space } from 'antd';
+import { Select, Radio, Divider, Space } from "antd";
 import Button from "../../components/Button";
-import { siteLocationList, tagList } from '../../constants/constants';
+import { siteLocationList, tagList } from "../../constants/constants";
 import tag_blue from "../../assets/icons/tag_blue.svg";
 import location_red from "../../assets/icons/location_red.svg";
 import { useSelector, useDispatch } from "react-redux";
@@ -20,14 +20,13 @@ const SearchPage = () => {
   const dispatch = useDispatch();
   const storyList = useSelector((state) => state.storyList.storyList);
 
-  const [selectedLocation, setSelectedLocation] = useState('');
-  const [selectedTag, setSelectedTag] = useState('');
-  const [selectedAuthor, setSelectedAuthor] = useState('all');
-  const [selectedDate, setSelectedDate] = useState('anytime');
+  const [selectedLocation, setSelectedLocation] = useState("");
+  const [selectedTag, setSelectedTag] = useState("");
+  const [selectedAuthor, setSelectedAuthor] = useState("all");
+  const [selectedDate, setSelectedDate] = useState("anytime");
   const [isFilterOpen, setIsFilterOpen] = useState(false);
   const [filteredStories, setFilteredStories] = useState([]);
   // const [previousFilters, setPreviousFilters] = useState({});
-
 
   useEffect(() => {
     const unsubscribe = dispatch(subscribeToStoryList(selectedTag)); // Pass selectedTag
@@ -38,18 +37,27 @@ const SearchPage = () => {
     setIsFilterOpen(!isFilterOpen);
   };
 
-  const filterStories = (storyList, selectedTag, selectedLocation, selectedAuthor, selectedDate) => {
+  const filterStories = (
+    storyList,
+    selectedTag,
+    selectedLocation,
+    selectedAuthor,
+    selectedDate
+  ) => {
     return storyList.filter((story) => {
+      const matchesLocation = selectedLocation
+        ? story.site && story.site === selectedLocation
+        : true;
 
-      const matchesLocation = selectedLocation ? (story.site && story.site.includes(selectedLocation)) : true;
-
-      const matchesTag = selectedTag ? (story.tags && story.tags.includes(selectedTag)) : true;
+      const matchesTag = selectedTag
+        ? story.tags && story.tags.includes(selectedTag)
+        : true;
 
       let matchesAuthor = true;
-      if (selectedAuthor === 'user') {
-        matchesAuthor = story.postType !== 'admin';
-      } else if (selectedAuthor === 'detroitRiverStoryLab') {
-        matchesAuthor = story.postType === 'admin';
+      if (selectedAuthor === "user") {
+        matchesAuthor = story.postType !== "admin";
+      } else if (selectedAuthor === "detroitRiverStoryLab") {
+        matchesAuthor = story.postType === "admin";
       }
 
       let matchesDate = true;
@@ -59,16 +67,18 @@ const SearchPage = () => {
         const daysDifference = (today - storyDate) / (1000 * 60 * 60 * 24); // Calculate difference in days
 
         switch (selectedDate) {
-          case 'today':
+          case "today":
             matchesDate = daysDifference < 1;
             break;
-          case 'thisWeek':
+          case "thisWeek":
             matchesDate = daysDifference < 7;
             break;
-          case 'thisMonth':
-            matchesDate = today.getMonth() === storyDate.getMonth() && today.getFullYear() === storyDate.getFullYear();
+          case "thisMonth":
+            matchesDate =
+              today.getMonth() === storyDate.getMonth() &&
+              today.getFullYear() === storyDate.getFullYear();
             break;
-          case 'thisYear':
+          case "thisYear":
             matchesDate = today.getFullYear() === storyDate.getFullYear();
             break;
           default:
@@ -81,10 +91,9 @@ const SearchPage = () => {
       console.log("Matches Tag:", matchesTag);
       console.log("Selected site:", selectedLocation);
       console.log("Matches site:", matchesLocation);
-      console.log("match author", matchesAuthor)
-      console.log("match date", matchesDate)
+      console.log("match author", matchesAuthor);
+      console.log("match date", matchesDate);
 
-  
       return matchesLocation && matchesTag && matchesAuthor && matchesDate;
     });
   };
@@ -93,35 +102,47 @@ const SearchPage = () => {
     console.log("Selected Tag:", selectedTag);
     console.log("Selected site:", selectedLocation);
     console.log("Selected author", selectedAuthor);
-    console.log("selected date", selectedDate)
-    const filteredStories = filterStories(storyList, selectedTag[0],selectedLocation, selectedAuthor, selectedDate); // Pass selectedTag[0]
+    console.log("selected date", selectedDate);
+    const filteredStories = filterStories(
+      storyList,
+      selectedTag[0],
+      selectedLocation,
+      selectedAuthor,
+      selectedDate
+    ); // Pass selectedTag[0]
     setFilteredStories(filteredStories);
     setIsFilterOpen(!isFilterOpen);
   };
 
   const handleCancelFilter = () => {
     // Reset the selected values
-    setSelectedLocation('');
-    setSelectedTag('');
+    setSelectedLocation("");
+    setSelectedTag("");
     setSelectedAuthor(null);
     setSelectedDate(null);
-    setIsFilterOpen(!isFilterOpen)
+    setIsFilterOpen(!isFilterOpen);
   };
 
   const filterOption = (input, option) => {
-    if (typeof option === 'string') {
+    if (typeof option === "string") {
       return option.toLowerCase().includes(input.toLowerCase());
     } else {
-      return (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
+      return (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
     }
   };
 
   return (
     <div className={`page-container ${styles["search-page-container"]}`}>
       <div className={styles["new-navbar-container"]}>
-        <img src={back} alt="back" onClick={() => navigate(-1)}/>
-        <Input prefix={<img src={search} alt="search" style={{ width: "20px" }}/>} placeholder="input search keywords" allowClear onPressEnter={onSearch} style={{ width: "68vw", borderRadius: "50px" }} />
-        <img src={filter} alt="filter" onClick={handleFilterClick}/>
+        <img src={back} alt="back" onClick={() => navigate(-1)} />
+        <Input
+          prefix={<img src={search} alt="search" style={{ width: "20px" }} />}
+          placeholder="input search keywords"
+          allowClear
+          onPressEnter={onSearch}
+          style={{ width: "68vw", borderRadius: "50px" }}
+        />
+        <img src={filter} alt="filter" onClick={handleFilterClick} />
       </div>
       {isFilterOpen && (
         <div className={styles["filter-container"]}>
@@ -144,8 +165,7 @@ const SearchPage = () => {
               label: site.name,
             }))}
             className={styles["drop-down"]}
-          >
-          </Select>
+          ></Select>
           <Divider />
 
           <h3>Tag</h3>
@@ -165,22 +185,29 @@ const SearchPage = () => {
               label: tag.label,
             }))}
             className={styles["drop-down"]}
-          >
-          </Select>
+          ></Select>
           <Divider />
 
           <h3>Author</h3>
-          <Radio.Group onChange={(e) => setSelectedAuthor(e.target.value)} value={selectedAuthor}>
-          <Space direction="vertical">
-            <Radio value="all">All</Radio>
-            <Radio value="user">User</Radio>
-            <Radio value="detroitRiverStoryLab">Detroit River Story Lab *</Radio>
-          </Space>
+          <Radio.Group
+            onChange={(e) => setSelectedAuthor(e.target.value)}
+            value={selectedAuthor}
+          >
+            <Space direction="vertical">
+              <Radio value="all">All</Radio>
+              <Radio value="user">User</Radio>
+              <Radio value="detroitRiverStoryLab">
+                Detroit River Story Lab *
+              </Radio>
+            </Space>
           </Radio.Group>
           <Divider />
 
           <h3>Date</h3>
-          <Radio.Group onChange={(e) => setSelectedDate(e.target.value)} value={selectedDate}>
+          <Radio.Group
+            onChange={(e) => setSelectedDate(e.target.value)}
+            value={selectedDate}
+          >
             <Space direction="vertical">
               <Radio value="anytime">Anytime</Radio>
               <Radio value="today">Today</Radio>
@@ -191,7 +218,11 @@ const SearchPage = () => {
           </Radio.Group>
 
           <div className={styles["filter-button"]}>
-            <Button text="Cancel Filter" handleOnClick={handleCancelFilter} customStyles={{ backgroundColor: '#D7D7D7' }} />
+            <Button
+              text="Cancel Filter"
+              handleOnClick={handleCancelFilter}
+              customStyles={{ backgroundColor: "#D7D7D7" }}
+            />
             <Button text="Apply Filter" handleOnClick={handleApplyFilter} />
           </div>
         </div>
@@ -215,7 +246,7 @@ const SearchPage = () => {
         )}
       </div>
     </div>
-  )
+  );
 };
 
 export default SearchPage;
