96dabb3e3cead3756d85115568f54bd6e92d7b7b
Xinran <xinrancao@hotmail.com>
2024-04-16 20:28:52 -0400

    bug fix: delete user

--- src/data/features/userInfoSlice.js
+++ src/data/features/userInfoSlice.js
@@ -10,8 +10,9 @@ import {
   collection,
   where,
   arrayRemove,
+  deleteDoc,
 } from "firebase/firestore";
-import { getAuth, signOut } from "firebase/auth";
+import { deleteUser, getAuth, signOut } from "firebase/auth";
 
 const initialState = {
   user: JSON.parse(localStorage.getItem("userInfo")) || null,
@@ -115,6 +116,26 @@ export const removeFromBookmarks = createAsyncThunk(
   }
 );
 
+export const deletUser = createAsyncThunk("userInfo/deleteUser", async () => {
+  try {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const userId = user.uid;
+
+    deleteUser(user)
+      .then(async () => {
+        const userRef = doc(db, "user", userId);
+        await deleteDoc(userRef);
+        console.log("User deleted successfully");
+      })
+      .catch((error) => {
+        console.log("Error deleting user:", error);
+      });
+  } catch (error) {
+    console.log("Error deleting user:", error);
+  }
+});
+
 const userInfoSlice = createSlice({
   name: "userInfo",
   initialState,
@@ -166,6 +187,18 @@ const userInfoSlice = createSlice({
       })
       .addCase(removeFromBookmarks.rejected, (state, action) => {
         state.status = "failed to remove from bookmarks";
+        state.error = action.error.message;
+      })
+      .addCase(deletUser.pending, (state) => {
+        state.status = "deleting user";
+      })
+      .addCase(deletUser.fulfilled, (state) => {
+        state.user = null;
+        localStorage.removeItem("userInfo");
+        state.status = "deleted user";
+      })
+      .addCase(deletUser.rejected, (state, action) => {
+        state.status = "failed to delete user";
         state.error = action.error.message;
       });
   },
